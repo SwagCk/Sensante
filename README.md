2958bf8413a33ba565e76b73aca4c1948177fe34
Awa Ndiaye <awacheikhndiaye06@gmail.com>
2026-04-06 15:10:52 +0000

    Lab 1 : Structure projet + dataset + exploration

---
+++ README.md
@@ -0,0 +1,23 @@
+#Sensante
+
+Assistant de pre-diagnostique medical pour le Senegal 
+
+##Description 
+
+SenSante utilise le Machine Learning pour aider au pre-diagnostic des maladies courantes (paludisme , grippe , typhoide) a partir des symptomes du patient.
+
+##Structure du projet
+
+- 'data/' : Donnees patients (CSV)
+- 'models/': Modele ML serialise
+- 'api/' : API FastAPI
+- 'frontend/' : Interface web
+- 'notebooks/' : Scripts d'exploitation
+
+
+##Auteur
+
+Awa Cheikh Ndiaye - L2 GLSI - ESP/UCAD
+
+##Cours 
+Integration de Modele IA - Dr.El Hadj Bassirou Toure
---
+++ notebooks/exploration.py
@@ -0,0 +1,49 @@
+import pandas as pd 
+
+#Charger les données
+
+df = pd.read_csv("dat/patients_dakar.csv")
+
+#Premier apercu du dataset
+print("=" * 50)
+print("SENSANTE - Exploration du dataset")
+print("=" * 50)
+
+#Dimensions du dataset
+print(f"\nNombre de patients : {len(df)}")
+print(f"Nombre de colonnes : {df.shape[1]}")
+print(f"Colonnes : {list(df.columns)}")
+
+
+#Apercu des 5 premières lignes
+print("\n-----Aperçu des 5 premières lignes-----")
+print(df.head())
+
+#Statistiques de base
+print(f"\n---Statistiques de descriptives---")
+print(df.describe().round(2))
+
+#Repartition des diagnostiques
+print(f"\n---Répartition des diagnostics---")
+diag_counts = df['diagnostic'].value_counts()
+for diag , count in diag_counts.items():
+    pct = count / len(df) * 100
+    print(f"{diag:12s} : {count:3d} patients ({pct:.1f}%)")
+
+#Repartition par region
+print(f"\n---Répartition par région(top 5)---")
+region_counts = df['region'].value_counts().head(5)
+for region, count in region_counts.items():
+    pct = count / len(df) * 100
+    print(f"{region:15s} : {count:3d} patients ({pct:.1f}%)")
+
+#Temperature moyenne par diagnostic
+print(f"\n---Température moyenne par diagnostic---")
+temp_by_diag = df.groupby('diagnostic')['temperature'].mean().round(2)
+for diag, temp in temp_by_diag.items():
+    print(f"{diag:12s} : {temp:.1f} °C")
+
+print(f"\n{'=' * 50}")
+print("Exploration terminée.")
+print(f"Prochain lab : entrainer un modele ML")
+print(f"{'=' * 50}")
